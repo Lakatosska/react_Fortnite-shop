72f37e5cde6845504aa1893f4a2cf1dbc892bba5
Lakatosska <vizualnoe@gmail.com>
2022-04-07 12:29:58 +0300

    added addToBasket function

--- src/components/GoodsItem.jsx
+++ src/components/GoodsItem.jsx
@@ -7,16 +7,17 @@ function GoodsItem(props) {
     <div className="card" id={mainId}>
       <div className="card-image">
       <img src={displayAssets[0].full_background} alt={displayName}/>
-        <span className="card-title">{displayName}</span>
+        
       </div>
       <div className="card-content">
+      <span className="card-title">{displayName}</span>
         <p>
           {displayDescription}
         </p>
       </div>
       <div className="card-action">
         <button className="btn">Купить</button>
-        <span className="right">{price.regularPrice}</span>
+        <span className="right" style={{fontSize: "1.8rem"}}>{price.regularPrice} руб.</span>
       </div>
     </div>        
     
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -2,14 +2,56 @@ import { useState, useEffect } from 'react';
 import { API_KEY, API_URL } from '../config';
 import { Preloader } from './Preloader';
 import { GoodsList } from './GoodsList';
+import { Cart } from './Cart';
 
 function Shop() {
 
   // список товаров и функция обновления этого списка
-  const [goods, setGoods] = useState('');
+  const [goods, setGoods] = useState([]);
   
   // сделаем состояние загрузки, задаем его изначально
   const [loading, setLoading] = useState(true);
+
+  // список заказов
+  const [order, setOrder] = useState([]);
+
+  // функция добавления товара в корзину, потом спускаем ее через пропсы в GoodsList
+  const addToBasket = (item) => {
+
+    // обходим наши заказы методом findIndex, если не находим индекс товара в индексе заказанного товара, то...
+    const itemIndex = order.findIndex(orderItem => orderItem.id === item.id)
+
+    if (itemIndex < 0) {
+      // ... товар добавляется первый раз (поэтому нужна проверка, чтоб quantity наращивать)
+      // объект ...item наследует все ключи, которые мы получим через ф-цию, и добавим поле quantity c "1"
+      const newItem = {
+        ...item,
+        quantity: 1,
+      }
+      // возвращает массив, который уже сейчас есть, и добавляет туда новый объект
+      // т.о. у нас массив объектов и каждый раз добавляем 1 новый
+      setOrder([...order, newItem]);
+
+      // если товар уже в корзине, то тогда изменяем значение quantity, все остальные значения - без изменений
+      // т.о. нужно перебрать наш массив
+    } else {
+      const newOrder = order.map((orderItem, index) => {
+
+        if(index === itemIndex) {
+          return {
+            ...orderItem,
+            quantity: orderItem.quantity + 1
+          }
+
+        } else {
+          return orderItem;
+        }
+      })
+
+      // в результате сформированный массив через метод setOrder отправляем в state
+      setOrder(newOrder);
+    }
+  }
 
   // всегда получает функцию и массив зависимостей(он пустой, т.к. надо выполнить 1 раз)
   useEffect(function getGoods() {
@@ -19,17 +61,14 @@ function Shop() {
       },
     }).then(res => res.json())
       .then(data => {
-        
         data.shop && setGoods(data.shop); // проверка, что данные пришли, иначе станут underfined
-        console.log(data.shop)
         setLoading(false); // в любом случае загрузка закончилась
       })
   }, [])
 
   return <main className='container content'>
-      {
-        loading ? <Preloader /> : <GoodsList goods={goods}/>
-      }
+      <Cart quantity={order.length} />
+      {loading ? <Preloader /> : <GoodsList goods={goods} addToBasket={addToBasket}/>}
     </main>
 }
 
